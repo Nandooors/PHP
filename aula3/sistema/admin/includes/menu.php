bcbec0fba9ae91878605684ba36c1e0cba6146a5
03849169022 <03849169022@senacrs.edu.br>
2024-06-19 21:36:29 -0300

    aula sisteminha

---
+++ aula3/sistema/admin/includes/menu.php
@@ -0,0 +1,30 @@
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" href="index.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="cadastrocliente.php">Cadastro cliente</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="cadastrofornecedor.php">Cadastro Fornecedor</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="cadastroproduto.php">CAdastro Produto</a>
+  </li>
+  <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Cadastros
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="cadastropais.php">Pais</a>
+          <a class="dropdown-item" href="cadastroestado.php">Estado</a>
+          <a class="dropdown-item" href="cadastrocidade.php">Cidade</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Desativado</a>
+      </li>
+  <li class="nav-item">
+    <a class="nav-link" href="sair.php">Sair</a>
+  </li>
+</ul>
